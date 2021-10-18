624cdbe87725d9a4336de2e9a5e96975ed679134
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-18 15:07:32 +0200

    Update distancematrix.java

--- Lab_1/distancematrix.java
+++ Lab_1/distancematrix.java
@@ -4,7 +4,9 @@ public class LinkedList{
     public class distancematrix {
         private LinkedList<GeometricPoint> cities;
         private LinkedList<LinkedList<Double>> Matrix;
-        public void distancematrix(){
+        public void distancematrix(LinkedList<GeometricPoint> cities,LinkedList<LinkedList<Double>> Matrix){
+            cities=cities;
+            matrix=Matrix;
         }
         public void addCity(GeometricPoint p){
             cities.add(p);
