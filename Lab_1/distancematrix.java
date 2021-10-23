b36fdbbb7b335a09b7b51c821c73b4678962eef0
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-23 16:43:11 +0200

    Update distancematrix.java

--- Lab_1/distancematrix.java
+++ Lab_1/distancematrix.java
@@ -1,18 +1,32 @@
 import java.util.LinkedList;
 
-public class LinkedList{
-    public class distancematrix {
-        private LinkedList<GeometricPoint> cities;
-        private LinkedList<LinkedList<Double>> Matrix;
-        public void distancematrix(LinkedList<GeometricPoint> cities,LinkedList<LinkedList<Double>> Matrix){
-            cities=cities;
-            matrix=Matrix;
-        }
-        public void addCity(GeometricPoint p){
-            cities.add(p);
-        }
-        public String getCityName(Int index){
+public class distancematrix {
+    private LinkedList<GeometricPoint> cities;
+    private LinkedList<LinkedList<Double>> matrix;
 
-
+    public distancematrix() {
+        cities = new LinkedList<GeometricPoint>();
+        matrix = new LinkedList<LinkedList<Double>>();
+    }
+    public void addCity(double InitX, double InitY, String Name){
+        GeometricPoint P = new GeometricPoint(InitX, InitY, Name);
+        cities.add(P);
+    }
+    public String getCityName(int index){
+        return cities.get(index).getname();
+    }
+    public int getNoOfCities(){
+        int big=cities.size();
+        return big;
+    }
+    public void createDistanceMatrix(){
+        int length = cities.size();
+        for(int i = 0; i < length; i++) {
+            List<Integer> list = new LinkedList<Integer>();
+            for(int j = 0; j < length; j++) {
+                list.add(0);
+            }
+            matrix.add(list);
+        }
     }
 }
