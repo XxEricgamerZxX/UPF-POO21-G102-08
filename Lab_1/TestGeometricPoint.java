1679acbfbd5e696a2f9dbd9e2e8047ded06f0ba9
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-25 22:14:23 +0200

    Update TestGeometricPoint.java

--- Lab_1/TestGeometricPoint.java
+++ Lab_1/TestGeometricPoint.java
@@ -3,6 +3,11 @@ public class TestGeometricPoint {
         GeometricPoint Point1 = new GeometricPoint (2.0,2.0, "Barcelona");
         GeometricPoint Point2 = new GeometricPoint (4.0,4.0, "Madrid");
 
+        System.out.println(Point1.getX());
+        System.out.println(Point1.getY());
+        System.out.println(Point1.getname());
         System.out.println(Point1.distance(Point2));
+        Point1.setX(3.0);
+        System.out.println(Point1.getX());
     }
 }
