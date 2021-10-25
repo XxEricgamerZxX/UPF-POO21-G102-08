6c02289a57b3bc2adc2bbbe4470b12ecd5b84b3b
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-25 23:25:04 +0200

    Update Testdistancematrix.java

--- Lab_1/Testdistancematrix.java
+++ Lab_1/Testdistancematrix.java
@@ -1,4 +1,4 @@
-public class Testdistancematrix {
+public class TestDistanceMatrix {
     public static void main (String[] args){
         // We create te points with them coordinates and names(of cities because the aim of the lab)
         GeometricPoint Point1 = new GeometricPoint (2.0,2.0, "Barcelona");
@@ -18,23 +18,6 @@ public class Testdistancematrix {
         // we print for any city name and the number of cities to check that the cities are correctly added.
         System.out.println(matrix.getCityName(1));
         System.out.println(matrix.getNoOfCities());
-        // As we can see, we can print each distance one by one but we prefered to do a for and print all of them.
-        for(int i=0;i<matrix.getNoOfCities();i++){
-            for(int j=0;j<matrix.getNoOfCities();j++){
-                System.out.println(matrix.getDistance(i,j));
-            }
-        }
-        /* *
-        System.out.println(matrix.getDistance(0,0));
-        System.out.println(matrix.getDistance(0,1));
-        System.out.println(matrix.getDistance(0,2));
-        System.out.println(matrix.getDistance(0,3));
-        System.out.println(matrix.getDistance(1,1));
-        System.out.println(matrix.getDistance(1,2));
-        System.out.println(matrix.getDistance(1,3));
-        System.out.println(matrix.getDistance(2,2));
-        System.out.println(matrix.getDistance(2,3));
-        System.out.println(matrix.getDistance(3,3));
-        /** */
+        
     }
 }
