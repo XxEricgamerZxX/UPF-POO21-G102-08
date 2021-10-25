9fc380a5d4945bc81417ebcf4b072d97ce8def87
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-25 23:24:24 +0200

    Update distancematrix.java

--- Lab_1/distancematrix.java
+++ Lab_1/distancematrix.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
 
-public class distancematrix {
+public class distancematrix implements Matrix{
+
     private LinkedList<GeometricPoint> cities = new LinkedList<GeometricPoint>();
     // We crate as atributes the Linkedlist of cities and for the matrix we decide to make a double array instead of double LinkedList.
     private double[][] matrix;
@@ -20,13 +21,20 @@ public class distancematrix {
         return big;
     }
     public void createDistanceMatrix(){
-        int length = this.getNoOfCities();
+        int length = cities.size();
         matrix = new double[length][length];
-        
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                matrix[i][j] = this.getDistance(i, j);
+            }
+        }
     }
+        
+    
     public double getDistance( int index1 , int index2 ){
         matrix[index1][index2] = cities.get(index1).distance(cities.get(index2));
         double Distance= matrix[index1][index2];
+        System.out.println("The Distance between " + cities.get(index1).getname() + " and " + cities.get(index2).getname() + " is: " + Distance);
         return Distance;
     }
 
