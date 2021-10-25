145c2e714249bab49a5e54a8383b0f9c8ab851f1
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-25 22:14:31 +0200

    Update distancematrix.java

--- Lab_1/distancematrix.java
+++ Lab_1/distancematrix.java
@@ -2,11 +2,12 @@ import java.util.LinkedList;
 
 public class distancematrix {
     private LinkedList<GeometricPoint> cities = new LinkedList<GeometricPoint>();
+    // We crate as atributes the Linkedlist of cities and for the matrix we decide to make a double array instead of double LinkedList.
     private double[][] matrix;
-
+    //We put the constructor that is a default constructor.
     public distancematrix() {
-    
     }
+    
     public void addCity(double InitX, double InitY, String Name){
         GeometricPoint P = new GeometricPoint(InitX, InitY, Name);
         cities.add(P);
@@ -21,16 +22,12 @@ public class distancematrix {
     public void createDistanceMatrix(){
         int length = this.getNoOfCities();
         matrix = new double[length][length];
-        for(int i=0;i<length;i++){
-            for(int j=0;j<length;j++){
-                matrix[i][j]= this.getDistance(i,j);
-            }
-        }
         
     }
     public double getDistance( int index1 , int index2 ){
         matrix[index1][index2] = cities.get(index1).distance(cities.get(index2));
-        return matrix[index1][index2];
+        double Distance= matrix[index1][index2];
+        return Distance;
     }
 
 }
