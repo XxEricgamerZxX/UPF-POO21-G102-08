df2a77c3f1f86f7a8bae81561472e0d1ea5911a5
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-25 23:25:09 +0200

    Create TestDisplayMatrix.java

---
+++ Lab_1/TestDisplayMatrix.java
@@ -0,0 +1,8 @@
+
+public class TestDisplayMatrix {
+    public static void main(String[] args) {
+        distancematrix matrix = new distancematrix();
+        DisplayMatrix display = new DisplayMatrix(matrix);
+        display.setVisible(true);
+    }
+}
