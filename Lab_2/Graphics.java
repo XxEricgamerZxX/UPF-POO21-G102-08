d4ad3d5d03d2393f29b34b966cc9a49fcb665bec
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-27 18:23:05 +0200

    Create Graphics.java

---
+++ Lab_2/Graphics.java
@@ -0,0 +1,10 @@
+import java.Graphics;
+
+public class Graphics {
+    public Graphics(){
+    }
+    public void drawString(String s, int X, int Y){
+    }
+    public void drawLine(int X1, int X2, int Y1, int Y2){
+    }
+}
