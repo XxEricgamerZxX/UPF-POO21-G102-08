dac9dca2081541ede8511a8c699027699b7f8d4a
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-10-27 18:23:10 +0200

    Create PolygonalRegion.java

---
+++ Lab_2/PolygonalRegion.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class PolygonalRegion {
+    private Point [] Points;
+
+    public PolygonalRegion(Point [] Points){
+        this.Points=Points;
+    }
+    public double getArea(){
+        int lenght = 7;
+       for (int i=0; i< lenght; i++){
+            Points.add(Point);
+        }
+        return 1.0;
+    }
+    public void draw( Graphics G, double X, double Y){
+    }
+}
