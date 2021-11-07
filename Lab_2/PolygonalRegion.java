f4dcfd13d0bc874597c9a94fd355756b2351719e
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-07 10:29:07 +0100

    Update PolygonalRegion.java

--- Lab_2/PolygonalRegion.java
+++ Lab_2/PolygonalRegion.java
@@ -1,19 +1,20 @@
 import java.util.LinkedList;
 
 public class PolygonalRegion {
-    private LinkedList<Point> Points;
+    private LinkedList<Point> Points= new LinkedList<Point>();;
 
-    public PolygonalRegion(LinkedList<Point> Points){ 
-        LinkedList<Point> Puntos= new LinkedList<Point>();
-        this.Points=Puntos;
+    public PolygonalRegion(LinkedList<Point> Puntos){ 
+        
     }
     public double getArea(){
         double area = 0.0;
-        int lenght = Points.size();
-        for (int i=0; i < lenght-1; i++){
-            area += (Points.get(i).getX()*Points.get(i+1).getY())-(Points.get(i).getY()*Points.get(i+1).getX()) ;
+        int length = Points.size();
+        for (int i=0; i < length-1; i++){
+            area += 1/2*((Points.get(i).getX()*Points.get(i+1).getY())-(Points.get(i).getY()*Points.get(i+1).getX())) ;
+            
         }
-        area += (Points.get(lenght).getX()*Points.get(lenght+1).getY())-(Points.get(lenght).getY()*Points.get(lenght+1).getX()) ;
+        area += 1/2*((Points.get(length-2).getX()*Points.get(0).getY())-(Points.get(length-2).getY()*Points.get(0).getX()));
+        
         return area;
     }
     public void draw( Graphics G, double X, double Y){
