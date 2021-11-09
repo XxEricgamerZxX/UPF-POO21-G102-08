e38835f7ee2749a9d19c29352f28ec6f481d7a77
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-09 10:47:12 +0100

    Update PolygonalRegion.java

--- Lab_2/PolygonalRegion.java
+++ Lab_2/PolygonalRegion.java
@@ -4,7 +4,7 @@ public class PolygonalRegion {
     private LinkedList<Point> Points= new LinkedList<Point>();;
 
     public PolygonalRegion(LinkedList<Point> Puntos){ 
-        
+        this.Points= Puntos;
     }
     public double getArea(){
         double area = 0.0;
@@ -13,7 +13,7 @@ public class PolygonalRegion {
             area += 1/2*((Points.get(i).getX()*Points.get(i+1).getY())-(Points.get(i).getY()*Points.get(i+1).getX())) ;
             
         }
-        area += 1/2*((Points.get(length-2).getX()*Points.get(0).getY())-(Points.get(length-2).getY()*Points.get(0).getX()));
+        area += 1/2*((Points.get(length-1).getX()*Points.get(0).getY())-(Points.get(length-1).getY()*Points.get(0).getX()));
         
         return area;
     }
