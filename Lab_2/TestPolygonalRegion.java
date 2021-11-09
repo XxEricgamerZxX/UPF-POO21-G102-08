092feb5c63f2b89fb799e5ae8917ebb52806f0dc
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-09 23:33:08 +0100

    Update TestPolygonalRegion.java

--- Lab_2/TestPolygonalRegion.java
+++ Lab_2/TestPolygonalRegion.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 public class TestPolygonalRegion {
     public static void main (String[] args){
         Point Punt1 = new Point(2.0,5.0);
-        Point Punt2 = new Point(-3.0,3.0);
+        Point Punt2 = new Point(-4.0,3.0);
         Point Punt3 = new Point(5.0,1.0);
 
         LinkedList<Point> Points= new LinkedList<Point>();
