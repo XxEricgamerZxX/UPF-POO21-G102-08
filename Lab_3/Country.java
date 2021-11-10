f28e720ecf0dce2ca09e510f20eb50db5a2d0dd3
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-10 17:58:01 +0100

    Create Country.java

---
+++ Lab_3/Country.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+public class Country {
+    private String name;
+    private LinkedList<City> Cities = new LinkedList<City>();
+    private LinkedList<Country> neighbors =new LinkedList<Country>();
+    private City Capital;
+
+    public void Country(LinkedList<Point> Puntos,City cap){
+        this.Capital=cap; 
+    }
+    
+}
