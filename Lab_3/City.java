162d979ae1971ffa44fcc63c76e5f4c01f50a35f
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-11 20:06:59 +0100

    Update City.java

---
+++ Lab_3/City.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+public class City {
+    private int numhab;
+
+    public void City(int X, int Y, int H, String name){
+
+    }
+
+    public void draw(Graphics G){
+
+    }
+}
