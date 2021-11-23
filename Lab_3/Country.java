4e8b5bb47e57aef81886d5644c66a77d14354b2f
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-23 12:00:15 +0100

    Update Country.java

--- Lab_3/Country.java
+++ Lab_3/Country.java
@@ -1,12 +1,32 @@
 import java.util.LinkedList;
+import java.awt.Graphics;
 public class Country {
+
     private String name;
-    private LinkedList<City> Cities = new LinkedList<City>();
-    private LinkedList<Country> neighbors =new LinkedList<Country>();
+    private City Cities [] ;
+    private Country neighbors [] ; 
     private City Capital;
 
-    public void Country(LinkedList<Point> Puntos,City cap){
+    public void Country( LinkedList<Point> Puntos,City cap){
         this.Capital=cap; 
     }
 
+    public void addCity(City c){
+        int size=0;
+        Cities[size]=c;
+        size = size + 1;
+    }
+    public void addneighbor( Country country){
+        int size=0;
+        neighbors[size]=country;
+		size = size + 1;
+    }
+
+    public void drawCountry(Graphics G){
+
+        for(int i=0; i< countries.size(); i++){
+            (countries.get(i)).draw(G);
+        }
+
+    }
 }
