c1093cb457d034eeebcc849defa5cc61d2c1d757
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-23 20:02:23 +0100

    Update City.java

--- Lab_3/City.java
+++ Lab_3/City.java
@@ -1,12 +1,31 @@
 import java.awt.*;
-public class City {
-    private int numhab;
 
-    public void City(int X, int Y, int H, String name){
+public class City extends GeoPoint {
+    protected int numhab;
 
+    public City(double X, double Y, int H, String name){
+        super(X, Y, name);
+        numhab=H;
+    }
+    public int getNumHab() {
+        return numhab;
     }
 
-    public void draw(Graphics G){
-
+    public void drawCity(Graphics G, int X, int Y, boolean Capital){
+        if ( Capital ) {
+            G.setColor(Color.red);
+            G.fillOval( X, Y, 7, 7 );
+            G.setColor(Color.blue);
+            G.setFont(new Font("AvantGarde", Font.PLAIN | Font.BOLD, 10));
+            G.drawString( name , X, Y );
+        } else {
+            G.setColor(Color.green);
+            G.fillOval( X, Y, 7, 7 );
+            G.setColor(Color.blue);
+            G.setFont(new Font("AvantGarde", Font.PLAIN, 10));
+            G.drawString( name , X, Y );
+        }
+        G.setColor(Color.black);
+        G.drawOval( X, Y, 7, 7 );
     }
 }
