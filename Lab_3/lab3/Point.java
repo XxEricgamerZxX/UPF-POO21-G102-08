476446a2f3ffc92c2f2a3e02cb8efe1b6eb877ee
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-28 14:02:46 +0100

    Create Point.java

---
+++ Lab_3/lab3/Point.java
@@ -0,0 +1,22 @@
+package lab3;
+public class Point {
+    protected double X;
+    protected double Y;
+
+    public Point(double InitX,double InitY){
+        X= InitX;
+        Y= InitY;
+    }
+    public double getX(){
+        return X;
+    }
+    public double getY(){
+        return Y;
+    }
+    public void setX( double X){
+        this.X=X;
+    }
+    public void setY( double Y){
+        this.Y=Y;
+    }
+}
