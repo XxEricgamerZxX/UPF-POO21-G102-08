7bd24edd59d0e2e61832ba0210d4e2bb1d9780b3
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-28 14:02:49 +0100

    Create PolygonalRegion.java

---
+++ Lab_3/lab3/PolygonalRegion.java
@@ -0,0 +1,41 @@
+package lab3;
+import java.util.LinkedList;
+import java.awt.*;
+
+public class PolygonalRegion extends Region {
+    public LinkedList<Point> Points= new LinkedList<Point>();
+
+    public PolygonalRegion(LinkedList<Point> Puntos){ 
+        this.Points= Puntos;
+    }
+
+    public double getArea(){
+        double area= 0.0;
+        int length = Points.size();
+        
+        for (int i=0; i < length-1; i++){
+            area += (Points.get(i).getX()*Points.get(i+1).getY()-Points.get(i).getY()*Points.get(i+1).getX())/2.0; 
+        } 
+        area += (Points.get(length-1).getX()*Points.get(0).getY()-Points.get(length-1).getY()*Points.get(0).getX())/2.0;       
+        return area;
+    }
+
+    public  void drawRegion( Graphics G){
+        int x[] = new int[Points.size()];
+        int y[] = new int[Points.size()];
+        int index = 0;
+
+        for(int i=0; i < Points.size(); i++){
+            x[i] = (int) (Points.get(i).getX());
+            y[i] = (int) (Points.get(i).getY());
+            index += 1;
+        } 
+        G.setColor(Color.white);
+        G.fillPolygon( x, y,index );
+        G.setColor(Color.yellow);
+        G.drawPolygon( x, y, index );
+        
+    
+    }
+
+}
