a0d4f8df9946af2f2e1829a10fc2af6696e6507e
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-28 14:02:37 +0100

    Create Country.java

---
+++ Lab_3/lab3/Country.java
@@ -0,0 +1,49 @@
+package lab3;
+
+import java.util.LinkedList;
+
+
+import java.awt.Graphics;
+
+public class Country extends PolygonalRegion {
+
+    private String name;
+    private LinkedList<City> Cities = new LinkedList<City>();
+    private Country neighbors [] ; 
+    private City Capital;
+
+    public Country(String name, LinkedList<Point> Puntos, City cap){
+        super(Puntos);
+        this.Capital=cap;
+        this.name= name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return Capital.getName();
+    }
+
+    public void addCity(City c){
+        Cities.add(c);
+    }
+
+    public void addneighbor( Country country){
+        int size=0;
+        neighbors[size]=country;
+		size = size + 1;
+    }
+
+    public void drawCountry( Graphics g) {
+        for ( int i = 0; i < Cities.size(); i++) {
+            boolean isCapital = false;
+            if ( (Cities.get(i)).getName() == Capital.getName() ) {
+                isCapital = true;
+            }
+            (Cities.get(i)).drawCity( g, (int)(Cities.get(i)).getX(), (int)(Cities.get(i)).getY(), isCapital );
+        }
+    }
+}
