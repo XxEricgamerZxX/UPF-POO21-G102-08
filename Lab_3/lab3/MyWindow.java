95d7760d7fc739c29eadeed13219c3579593a3fb
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-28 14:02:44 +0100

    Create MyWindow.java

---
+++ Lab_3/lab3/MyWindow.java
@@ -0,0 +1,26 @@
+package lab3;
+
+public class MyWindow extends javax.swing.JFrame {
+
+    public MyWindow() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setDefaultCloseOperation( javax.swing.WindowConstants.EXIT_ON_CLOSE );
+    }
+
+    public static void main( String[] args ) {
+        java.awt.EventQueue.invokeLater( new Runnable() {
+            public void run() {
+                MyWindow w = new MyWindow();
+                MyMap m = new MyMap();
+                w.add( m );
+                w.setVisible( true );
+                w.pack();
+            }
+        } );
+    }
+
+}
+
