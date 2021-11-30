ef8355f77268be97f29ddfa51e7a18d8416b712e
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-30 10:02:49 +0100

    Update Region.java

--- Lab_4/lab4/Region.java
+++ Lab_4/lab4/Region.java
@@ -4,14 +4,17 @@ import java.awt.*;
 public abstract class Region extends Entity {
     protected Color fillcolor;
     
-    public Region(){
+    public Region(Color InitlineC, Color InitFill){
+        super(InitlineC);
+        fillcolor= InitFill;
     }
-    public void setFillColor( Color InitColor){
-        InitColor = fillcolor;
+    public void setFillColor( Color InitFill){
+        InitFill = fillcolor;
     }
 
-    abstract double getArea();
-
-    abstract Boolean isPointinside(Point p);
+    abstract public double getArea();
+    abstract public void draw( Graphics g );
+    abstract public Boolean isPointinside(Point p);
+    abstract public void translate( int dx, int dy );
 
 }
