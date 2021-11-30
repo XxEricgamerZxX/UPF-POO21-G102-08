6790a5fda15696e585b32c9f801f893461aa7dc5
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-30 10:02:47 +0100

    Create RectangularRegion.java

---
+++ Lab_4/lab4/RectangularRegion.java
@@ -0,0 +1,23 @@
+package lab4;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.awt.*;
+
+public class RectangularRegion extends Polygonalregion {
+    private LinkedList<Point> Puntos;
+
+    public RectangularRegion( Color lineColor, Color fillColor,Point p1, Point p2) {
+        super(lineColor, fillColor, new LinkedList<Point>(Arrays.asList(p1,p2,new Point(p1.getX(), p2.getY()), new Point(p2.getX(), p1.getY()) ) ));
+        Puntos.add( p1 );
+        Puntos.add( p2 );
+        Puntos.add(new Point(p1.getX(), p2.getY()));
+        Puntos.add(new Point(p2.getX(), p1.getY()));
+    }
+    public double getArea(){
+        
+        return super.getArea();
+
+    }
+    
+}
