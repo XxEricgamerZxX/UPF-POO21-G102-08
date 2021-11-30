b034bba65c13bdf14105b8837864e3300f5e9ff2
arsandiu <92306583+arsandiu@users.noreply.github.com>
2021-11-30 10:02:37 +0100

    Update EllipsoidalRegion.java

--- Lab_4/lab4/EllipsoidalRegion.java
+++ Lab_4/lab4/EllipsoidalRegion.java
@@ -8,39 +8,50 @@ public class EllipsoidalRegion extends Region{
     private double r1;
     private double r2;
 
-    public EllipsoidalRegion(Point initC,double initR1, double initR2){
-        super(); 
+    public EllipsoidalRegion(Color InitlineC, Color InitFill, Point initC, double initR1, double initR2){
+        super(InitlineC,InitFill); 
         this.c = initC;
         this.r1 = initR1;
         this.r2 = initR2;
     }
 
-    @Override
-    double getArea() {
+    
+    public double getArea() {
         double area = 0;
         area = Math.PI * this.r1 * this.r2;
         return area;
     }
 
-    @Override
-    Boolean isPointinside(Point p) {
-        // TODO Auto-generated method stub
-        return null;
+    
+    public Boolean isPointinside(Point p) {
+        
+        Boolean isInside = true;
+        double check = Math.pow(p.getX()-c.getX(),2)/r1 + Math.pow(p.getY()-c.getY(),2)/r2;
+        if ( check <= 1.0){
+            return isInside;
+        }
+
+        else{
+            isInside= false;
+            return isInside;
+        }
+
     }
 
-    @Override
     public boolean isSelected(Point Punto) {
         // TODO Auto-generated method stub
         return false;
     }
 
-    @Override
+   
     public void draw(Graphics g) {
+        g.setColor( fillcolor );
+        g.fillOval( (int) this.c.getX(), (int) this.c.getY(), (int) r1, (int) r2 );
+        g.setColor( lineColor );
         g.drawOval((int) this.c.getX(), (int) this.c.getY(), (int) this.r1, (int) this.r2);
         
     }
 
-    @Override
     public void translate(int dx, int dy) {
         // TODO Auto-generated method stub
         
